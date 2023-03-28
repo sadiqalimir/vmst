8a24feb6ee7c63f4b10ea0e309e56b990293b0b3
Sadiq Ali <114666145+sadiqalimir@users.noreply.github.com>
2023-03-28 23:55:04 +0530

    new initial

---
+++ script.js
@@ -0,0 +1,6 @@
+// Add an event listener to the button
+const button = document.querySelector('.button');
+button.addEventListener('click', () => {
+  // Show a message when the button is clicked
+  alert('Thanks for downloading the tool!');
+});
